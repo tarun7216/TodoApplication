4e2889421d6fee45153ea3ac1ba7bbeb535023e7
TarunKumarP <tarun.kumar@thinkjs.co>
2022-05-24 10:23:20 +0530

    TodoApp completed without css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [textarea,setTextarea] = useState("This is my new text area to have");
+  
+  const handlechange = (e) => {
+    setTextarea(e.target.value)
+    
+  }
   return (
     <div className="App">
       <header className="App-header">
        <h1> Todo App </h1>
+       <form>
+         <textarea value={textarea} onChange={handlechange} /><br />
+         <input type="submit" value="Addtodo" className='' />
+       </form>
       </header>
     </div>
   );
