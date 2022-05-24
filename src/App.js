17a103e36139232761725d585a22f948ddfaf105
TarunKumarP <tarun.kumar@thinkjs.co>
2022-05-24 17:05:44 +0530

    onclick function code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Football from './codes/Onclick'
+import Form from './codes/Onclick1'
 
 const App = () => {
   return (
     <div>
-      
+      <Football />
+      <Form />
     </div>
   )
 }
