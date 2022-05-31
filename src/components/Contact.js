2e9775bc96ea0fabf6711b55c8926c469236da31
TarunKumarP <tarun.kumar@thinkjs.co>
2022-05-31 16:13:00 +0530

    initial commit

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+        <form>
+            <label>
+                FirstName
+                <input type="text" />
+            </label>
+        </form>
+      
+    </div>
+  )
+}
+
+export default Contact
